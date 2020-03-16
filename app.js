6c5528fe7636e226726c173bf434b085480c9214
LimongiVirgil <limongi.virgil@gmail.com>
2020-03-16 12:25:12 +0100

    add response

--- app.js
+++ app.js
@@ -9,4 +9,8 @@ app.get('/', function(req, res) {
   res.render('index');
 });
 
+app.post('/party', function(req, res) {
+  res.send('Post ok !')
+});
+
 app.listen(process.env.PORT, () => console.log(`Front app listening on port ${process.env.PORT}!`));
