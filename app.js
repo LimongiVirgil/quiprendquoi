774f194e3c10d87b8966f9c7014ba8d6ba67c653
LimongiVirgil <limongi.virgil@gmail.com>
2020-03-16 12:39:25 +0100

    add axios

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const dotenv = require('dotenv').config();
 const bodyParser = require('body-parser');
+const axios = require('axios');
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
@@ -14,7 +15,13 @@ app.get('/', function(req, res) {
 
 app.post('/party', function(req, res) {
   console.log(req.body);
-  res.send(res.send('Post ok !'))
+  
+  //res.send(res.send('Post ok !'))
+
+  axios
+    .post(`${process.env.API_URL}/party`, req.body)
+    .then(({data}) => console.log(data))
+    .catch((err) => console.error(err));
 });
 
 app.listen(process.env.PORT, () => console.log(`Front app listening on port ${process.env.PORT}!`));
