72648c3b79d5b8f8ff2050dfc61df526b302f4bb
LimongiVirgil <limongi.virgil@gmail.com>
2020-03-17 11:19:51 +0100

    add copy

--- scripts/clipboard.js
+++ scripts/clipboard.js
@@ -1,11 +1,27 @@
 if (navigator.clipboard) {
-  console.log("Support du presse papier")
   document.querySelectorAll('[data-clipboard]')
   document.querySelectorAll('[data-clipboard]').forEach(($clipboardEl) => {
     const $button = document.createElement('button');
     $button.innerHTML = 'Copier';
     $clipboardEl.parentNode.append($button);
+
+    $button.innerHTML = 'Copié !';
+    setTimeout(() => ($button.innerHTML = 'Copier'), 2000);
+
+    $button.addEventListener(
+      'click',
+      copyToClipboard.bind(this, $clipboardEl, $button)
+    );
   });
 } else {
   console.warn("Pas de support :(")
+}
+
+function copyToClipboard($clipboardEl, $button) {
+  navigator.clipboard
+  .writeText($clipboardEl.getAttribute('data-clipboard'))
+  .then(() => {
+    console.log('Copié !');
+  })
+  .catch((err) => console.warn(err));
 }
